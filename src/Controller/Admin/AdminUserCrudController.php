df162096f8191353085d74a20413cb3003ece4e0
Asier Zunzunegui <asier.zunzu@gmail.com>
2024-08-31 19:30:23 +0200

    Add user crud controller for admins

---
+++ src/Controller/Admin/AdminUserCrudController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Enum\Roles;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class AdminUserCrudController extends AbstractCrudController
+{
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityPermission(Roles::ADMIN->value);
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('username'),
+            EmailField::new('email'),
+            ChoiceField::new('roles')
+                ->allowMultipleChoices()
+                ->setFormTypeOption('choices', Roles::cases())
+                ->setFormTypeOption('choice_label', function (Roles $choice) {
+                    return $choice->name;
+                })
+                ->setFormTypeOption('choice_value', function (Roles|int|string|null $value): ?string {
+                    if (null === $value) {
+                        return null;
+                    }
+
+                    return (string) ($value instanceof Roles ? $value->value : $value);
+                }),
+        ];
+    }
+}
